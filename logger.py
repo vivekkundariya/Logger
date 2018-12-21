f9e8213250dd4b1533d6d99ee733c8802f0dbe68
vivekkundariya1996 <vivekkundariya1996@gmail.com>
2018-12-21 13:00:52 +0530

    [logging] Add log function update printing structure

--- logger.py
+++ logger.py
@@ -25,12 +25,25 @@ class logger:
         file_array = filename.split('/')
         file_array= file_array[len(file_array)-1].split("\\")
         s = ' '.join(str(i) for i in args)
-        print((file_array[len(file_array)-1], inspect.stack()[1][3]), " :-  ", s)
+        print((file_array[len(file_array)-1], inspect.stack()[2][3], inspect.stack()[1][3]), " :-  ", s)
         return
 
     def error(*args):
         logger.__print("error",*args)
         return
+    
+    def log(*args):
+        logger.__print(*args)
 
-
+if __name__ == "__main__":
+    logger.disableDebug()
+    logger.debug("hello")
+    logger.error("hello")
+    logger.log("normal", "log", "hello")
+    
+    logger.enableDebug()
+    logger.debug("hello")
+    logger.error("hello")
+    
+    
     
